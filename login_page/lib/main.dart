69b9d09e9424528c95fc3eac8b51df868e4f69b2
김진아 <c2010333@naver.com>
2024-05-24 21:43:07 +0900

    first commit

---
+++ login_page/lib/main.dart
@@ -0,0 +1,84 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.amber,
+            centerTitle: true,
+            title: Text(
+              "Hello Flutter",
+              style: TextStyle(fontSize: 26, color: Colors.black),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Image.network(
+                      "https://i.ibb.co/nngK6j3/startup.png",
+                      width: 80,
+                    ),
+                  ),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: "이메일",
+                        labelStyle:
+                            TextStyle(color: Colors.black, fontSize: 13),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.amber),
+                        ),
+                        contentPadding: EdgeInsets.all(10.0)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          labelText: "비밀번호",
+                          labelStyle:
+                              TextStyle(color: Colors.black, fontSize: 13),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.amber),
+                          ),
+                          contentPadding: EdgeInsets.all(10.0)),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(top: 24),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.amber),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.black),
+                      ),
+                      onPressed: () {},
+                      child: Text("로그인"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+//commit용
